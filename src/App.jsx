f4d29000c43fbe7f044b829111a224ed3a3bf278
khushalDevelops <khushalthakur.dev@gmail.com>
2025-07-13 19:37:40 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import Header from './components/header'
+import About from './components/About'
+import Projects from './components/Projects'
+import Testimonials from './components/Testimonials'
+import Contact from './components/Contact'
+import { ToastContainer, toast } from 'react-toastify';
+import Footer from './components/footer'
+
+const App = () => {
+  return (
+    <div className='w-full overflow-hidden'>
+      <ToastContainer/>
+      <Header/>
+      <About/>
+      <Projects/>
+      <Testimonials/>
+      <Contact/>
+      <Footer/>
+    </div>
+  )
+}
+
+export default App
